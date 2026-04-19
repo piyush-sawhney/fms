c050d893be6eb6d044221ee71dfcda79a6fb89c4
Piyush Sawhney <piyush.sawhney@knaps.in>
2026-04-19 11:15:20 +0000

    removing unneccsary code for now

--- fms/fms_core/doctype/fms_investor/fms_investor.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, KNAPS and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("FMS Investor", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- fms/fms_core/doctype/fms_investor/fms_investor.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, KNAPS and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FMSInvestor(Document):
-	pass
